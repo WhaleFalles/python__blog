0ff7a6b88fb548e8139fe74f7d3d66c391f59f6f
WhaleFalles <97121628@qq.com>
2018-07-29 15:59:20 +0800

    django学习之旅第1.5天
    完成了基本的博客主页文章的展示与动态url打开不同的文章的效果的实现

--- env/Scripts/my_blog/article/urls.py
+++ env/Scripts/my_blog/article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('a1/',views.detail,name='detail'),
+    path('<int:id>/',views.detail,name='detail'),
     path('',views.home,name='home'),
     path('index1/',views.index,name='index'),
  ]
--- env/Scripts/my_blog/article/views.py
+++ env/Scripts/my_blog/article/views.py
@@ -1,15 +1,20 @@
 # coding:utf-8
 from django.shortcuts import render
 from article.models import Article
+from datetime import datetime
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
 def home(request):
 	post_list=Article.objects.all()
 	return render(request,'article/home.html',{'post_list':post_list})
 
-def detail(request):
-    return HttpResponse("you are looking at my_args ")
+def detail(request,id):
+	try:
+		post=Article.objects.get(id=str(id))
+		return render(request,'article/post.html',{'post':post})
+	except:
+		return HttpResponse("hello")
 
 def index(request):
  	return render(request,'article/index.html')
