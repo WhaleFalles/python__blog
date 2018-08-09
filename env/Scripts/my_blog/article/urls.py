cf2e196cf01e850e604956f7d1add48667f2a0ee
WhaleFalles <97121628@qq.com>
2018-08-09 16:04:20 +0800

    complate

--- env/Scripts/my_blog/article/urls.py
+++ env/Scripts/my_blog/article/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from article.views import RSSFeed
 urlpatterns=[
     path('<int:id>/',views.detail,name='detail'),
     path('',views.home,name='home'),
     path('index1/',views.index,name='index'),
+    path('tag<str:tag>/',views.search_tag,name='search_tag'),
+    path('search/',views.blog_search,name='blog_search'),
+    path('archives/',views.archives,name='archives'),
+    path('feed/',RSSFeed,name='RSS'),
  ]
--- env/Scripts/my_blog/article/views.py
+++ env/Scripts/my_blog/article/views.py
@@ -1,13 +1,26 @@
 # coding:utf-8
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from article.models import Article
 from datetime import datetime
+from django.contrib.syndication.views import Feed
 # Create your views here.
 from django.http import HttpResponse,Http404
+from django.views.generic import View
+from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 def home(request):
-	post_list=Article.objects.all()
+	post=Article.objects.all()
+	paginator=Paginator(post,2)
+	page=request.GET.get('page')
+	try:
+		post_list=paginator.page(page)
+	except PageNotAnInteger:
+		post_list=paginator.page(1)
+	except EmptyPage:
+		post_list=paginator.paginator(paginator.num_pages)
 	return render(request,'article/home.html',{'post_list':post_list})
+	
 
 def detail(request,id):
 	try:
@@ -19,3 +32,53 @@ def detail(request,id):
 def index(request):
  	return render(request,'article/index.html')
 
+def search_tag(request,tag):
+ 	try:
+ 		post_list=Article.objects.filter(category__iexact=tag)
+ 		return render(request,'article/tag.html',{'post_list':post_list})
+ 	except Article.DoesNotExist:
+ 		raise Http404
+
+
+def blog_search(request):
+	if 'sss' in request.GET:
+	#return HttpResponse("wwwww")
+		
+		s=request.GET['sss']
+		if not s:
+			return render(request,'article/archives.html',{'post_list':post_list,'error':True})
+		else:
+			post_list=Article.objects.filter(title__icontains=s)
+			if len(post_list)==0:
+				return render(request,'article/archives.html',{'post_list':post_list,'error':True})
+			else:
+				return render(request,'article/archives.html',{'post_list':post_list,'error':False})
+			#return redirect('/')	
+	else:
+		return HttpResponse(request.GET.get('sss'))
+
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+
+class RSSFeed(Feed):
+	title="RSS feed-aritcle"
+	template_name="article/post.html"
+	desrciption="RSS feed - blog posts"
+    
+	def items(self):
+		return Article.objects.order_by('-date_time')
+
+	def item_title(self,item):
+		return item.title
+
+	def item_pubdate(self,item):
+		return item.add_date
+
+	def item_description(self,item):
+		return item.content
